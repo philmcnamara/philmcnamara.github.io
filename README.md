09f553e2dd774f41c15fc1f4383ce1ce88012afb
Phil McNamara <phil.j.mcnamara@gmail.com>
2026-03-17 14:16:38 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Portfolio Site
+
+This site contains links to some of my recent work, and also serves as a place for me to experiment with Tailwind and Astro
---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  site: 'https://philmcnamara.github.io',
+
+  integrations: [mdx()]
+});
